f517307ad5cfb41eac5686aded7b0162eca2187d
= <anirudh.b.123@gmail.com>
2019-05-09 14:07:12 +0530

    made responsive

--- js/main.js
+++ js/main.js
@@ -1,61 +1,60 @@
-const page1 = document.querySelector('#page1');
-const page2 = document.querySelector('#page2');
-const page3 = document.querySelector('#page3');
-const homeIcon = document.querySelector('.fa-home');
-const viewIcon = document.querySelector('.fa-eye');
-const contactIcon = document.querySelector('.fa-envelope');
-const contactButton = document.querySelector('#contactButton');
-const rightArrow = document.querySelector('.fa-arrow-right');
-const githubButton = document.querySelector('.fa-github');
-const linkedinButton = document.querySelector('.fa-linkedin');
-const roundButtons = document.querySelectorAll('.roundButton');
-const sendMessage = document.querySelector('#sendMessage');
+const $ = (element, index) => {
+	if (index) {
+		return document.querySelectorAll(element)[index];
+	}
+	return document.querySelector(element);
+}
 
 let currentPage = 1;
 
-contactButton.addEventListener('mouseover', () => {
-	rightArrow.style.transform = "rotate(90deg)";
-});
-contactButton.addEventListener('mouseout', () => {
-	rightArrow.style.transform = "rotate(0deg)";
-});
-
-contactButton.addEventListener('click', () => {
+$('#contactButton').addEventListener('click', () => {
+	$('#page3').scrollIntoView();
 	currentPage = 3;
 	iconHighLight();
 });
 
-githubButton.addEventListener('mouseout', () => {
-	window.open('https://github.com/anirudh1200', '_blank');
+window.addEventListener('ontouchmove', () => {
+	alert('done');
 });
 
-linkedinButton.addEventListener('mouseout', () => {
-	window.open('https://www.linkedin.com/in/anirudh-bhat-a303a2179/', '_blank');
-});
+for (let i = 0; i < 2; i++) {
+	$('.fa-github', i).addEventListener('click', () => {
+		window.open('https://github.com/anirudh1200', '_blank');
+	});
+	$('.fa-linkedin', i).addEventListener('click', () => {
+		window.open('https://www.linkedin.com/in/anirudh-bhat-a303a2179/', '_blank');
+	});
+	$('.roundButton', i).addEventListener('click', () => {
+		$('#page'+(i+2)).scrollIntoView();
+		currentPage = i + 2;
+		iconHighLight();
+	});
+}
 
-roundButtons[0].addEventListener('click', () => {
-	currentPage = 2;
-	iconHighLight();
-});
-
-roundButtons[1].addEventListener('click', () => {
-	currentPage = 3;
-	iconHighLight();
-});
-
-homeIcon.addEventListener('click', () => {
+$('.fa-home').addEventListener('click', () => {
+	$('#page1').scrollIntoView();
 	currentPage = 1;
 	iconHighLight();
 });
 
-viewIcon.addEventListener('click', () => {
+$('.fa-eye').addEventListener('click', () => {
+	$('#page2').scrollIntoView();
 	currentPage = 2;
 	iconHighLight();
 });
 
-contactIcon.addEventListener('click', () => {
+$('.fa-envelope').addEventListener('click', () => {
+	$('#page3').scrollIntoView();
 	currentPage = 3;
 	iconHighLight();
+});
+
+$('#hamburger').addEventListener('click', () => {
+	if ($('#navIconsBox').style.transform === 'translateX(0px)') {
+		$('#navIconsBox').style.transform = 'translateX(100%)';
+	} else {
+		$('#navIconsBox').style.transform = 'translateX(0)';
+	}
 });
 
 let wheelActive = true;
@@ -63,20 +62,20 @@ let wheelActive = true;
 window.addEventListener('wheel', e => {
 	if (e.deltaY < 0) {
 		if (currentPage === 2 && wheelActive) {
-			page1.scrollIntoView();
+			$('#page1').scrollIntoView();
 			currentPage = 1;
 		} else if (currentPage === 3 && wheelActive) {
-			page2.scrollIntoView();
+			$('#page2').scrollIntoView();
 			currentPage = 2;
 		}
 	}
 	if (e.deltaY > 0) {
 		if (currentPage === 1 && wheelActive) {
-			page2.scrollIntoView();
+			$('#page2').scrollIntoView();
 			currentPage = 2;
-			viewIcon.classList.add('iconSelected');
+			$('.fa-eye').classList.add('iconSelected');
 		} else if (currentPage === 2 && wheelActive) {
-			page3.scrollIntoView();
+			$('#page3').scrollIntoView();
 			currentPage = 3;
 		}
 	}
@@ -86,27 +85,27 @@ window.addEventListener('wheel', e => {
 });
 
 const unselectAll = () => {
-	homeIcon.classList.remove('iconSelected');
-	viewIcon.classList.remove('iconSelected');
-	contactIcon.classList.remove('iconSelected');
+	$('.fa-home').classList.remove('iconSelected');
+	$('.fa-eye').classList.remove('iconSelected');
+	$('.fa-envelope').classList.remove('iconSelected');
 }
 
 const iconHighLight = () => {
 	unselectAll();
 	if (currentPage === 1)
-		homeIcon.classList.add('iconSelected');
+		$('.fa-home').classList.add('iconSelected');
 	else if (currentPage === 2)
-		viewIcon.classList.add('iconSelected');
+		$('.fa-eye').classList.add('iconSelected');
 	else if (currentPage === 3)
-		contactIcon.classList.add('iconSelected');
+		$('.fa-envelope').classList.add('iconSelected');
 }
 
-sendMessage.addEventListener('click', e => {
+$('#sendMessage').addEventListener('click', e => {
 	e.preventDefault();
 	let json = new Object();
-	json.name = document.querySelector('#nameInput').value;
-	json.email = document.querySelector('#emailInput').value;
-	json.message = document.querySelector('#messageInput').value;
+	json.name = $('#nameInput').value;
+	json.email = $('#emailInput').value;
+	json.message = $('#messageInput').value;
 	let today = new Date();
 	json.date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
 	let jsonString = JSON.stringify(json);
@@ -116,12 +115,12 @@ sendMessage.addEventListener('click', e => {
 		if (req.readyState == XMLHttpRequest.DONE) {
 			let response = JSON.parse(req.responseText);
 			if (response.success = true) {
-				document.querySelector('#nameInput').value = '';
-				document.querySelector('#emailInput').value = '';
-				document.querySelector('#messageInput').value = '';
-				document.querySelector('#feedback').innerHTML = 'Message Sent Successfully';
+				$('#nameInput').value = '';
+				$('#emailInput').value = '';
+				$('#messageInput').value = '';
+				$('#feedback').innerHTML = 'Message Sent Successfully';
 			} else {
-				document.querySelector('#feedback').value = 'An Error Occured. Please free to contact me at my email.';
+				$('#feedback').value = 'An Error Occured. Please free to contact me at my email.';
 			}
 		}
 	};
